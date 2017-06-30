e6558a32d613ad988c07f815c5b13d7f1586f42e
System Administrator <root@skycityroomdeiMac.local>
2017-06-30 12:39:31 +0800

    port 80

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ app.get('/', function(req, res, next) {
     res.redirect('http://weibo.com/u/2720114643');
 });
 
-app.listen(8089, function(req, res) {
-    console.log('app is running at port 8089');
+app.listen(80, function(req, res) {
+    console.log('app is running at port 80');
 });
